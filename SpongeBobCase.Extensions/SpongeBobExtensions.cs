de9393e74be9e39036bc60777b2f964b7d49557a
Gabe Stocco <98900+gfs@users.noreply.github.com>
2022-06-08 04:22:18 -0700

    Fix gitattributes.

--- SpongeBobCase.Extensions/SpongeBobExtensions.cs
+++ SpongeBobCase.Extensions/SpongeBobExtensions.cs
@@ -5,12 +5,23 @@ namespace SpongeBobCase.Extensions;
 
 public static class SpongeBobExtensions
 {
+    /// <summary>
+    /// Convert a string to SpOnGeBoB sArCaSm CaSe using <see cref="CultureInfo.CurrentCulture"/>
+    /// </summary>
+    /// <param name="str">A string.</param>
+    /// <returns>ThE sAmE sTrInG, bUt sArCaStIc.</returns>
     public static string ToSpongeBobCase(this string str)
     {
         ArgumentNullException.ThrowIfNull(str, nameof(str));
         return str.ToSpongeBobCase(CultureInfo.CurrentCulture);
     }
     
+    /// <summary>
+    /// Convert a string to SpOnGeBoB sArCaSm CaSe using the specified <see cref="CultureInfo"/>
+    /// </summary>
+    /// <param name="str">A string.</param>
+    /// <param name="culture">The culture to use.</param>
+    /// <returns>ThE sAmE sTrInG, bUt sArCaStIc.</returns>
     public static string ToSpongeBobCase(this string str, CultureInfo culture)
     {
         ArgumentNullException.ThrowIfNull(str, nameof(str));
@@ -19,7 +30,13 @@ public static class SpongeBobExtensions
         return culture.TextInfo.ToSpongeBobCase(str);
     }
 
-    private static string ToSpongeBobCase(this TextInfo textInfo, string stringToSpongeBob)
+    /// <summary>
+    /// Convert a string to SpOnGeBoB sArCaSm CaSe using the specified <see cref="TextInfo"/>
+    /// </summary>
+    /// <param name="stringToSpongeBob">A string.</param>
+    /// <param name="textInfo">The culture to use.</param>
+    /// <returns>ThE sAmE sTrInG, bUt sArCaStIc.</returns>
+    public static string ToSpongeBobCase(this TextInfo textInfo, string stringToSpongeBob)
     {
         ArgumentNullException.ThrowIfNull(textInfo, nameof(textInfo));
         ArgumentNullException.ThrowIfNull(stringToSpongeBob, nameof(stringToSpongeBob));
